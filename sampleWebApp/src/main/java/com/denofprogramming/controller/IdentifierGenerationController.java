e8137fa9ccb74ebc381bc4afb2bf65e466adfb22
RavitejaAdepudi <38037950+RavitejaAdepudi@users.noreply.github.com>
2018-07-27 15:25:34 +0530

    Update IdentifierGenerationController.java

--- sampleWebApp/src/main/java/com/denofprogramming/controller/IdentifierGenerationController.java
+++ sampleWebApp/src/main/java/com/denofprogramming/controller/IdentifierGenerationController.java
@@ -1,7 +1,5 @@
 package com.denofprogramming.controller;
-
  javax.inject.Inject;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
@@ -37,13 +35,13 @@ import com.denofprogramming.random.RandomGenerator;
 @Controller
 public class IdentifierGenerationController 
 	
-	// Using the slf4j api directly - remove comments from this code only after SLF4J dependencies are added, see tutorials. 
+{	// Using the slf4j api directly - remove comments from this code only after SLF4J dependencies are added, see tutorials. 
 	// private static final Logger logger = LoggerFactory.getLogger(IdentifierGenerationController.class);
 
 	// using the log4j api, jar provided by our specific dependency
 	private static final Logger logger = Logger
 			.getLogger("com.denofprogramming.controller.IdentifierGenerationController");
-
+}
 	// using the JCL api, jar provided by transient dependency via spring-core
 	private Log log = LogFactory.getLog(IdentifierGenerationController.class);
 
